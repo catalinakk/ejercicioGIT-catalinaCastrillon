45e73ef0cdc8a1e02e3587d6ed2e1304cb89f56b
catalinakk0205 <33283617+catalinakk0205@users.noreply.github.com>
2021-11-25 11:38:54 -0500

    Add files via upload
    primera parte del examen practico

---
+++ examen practico.js
@@ -0,0 +1,73 @@
+//Ejercicio 1
+
+var 
+numero1 = 5;
+
+var 
+numero2 = 8;
+
+
+
+if(numero1 <=
+numero2) {s
+
+console.log("numero1 no es mayor que numero2");
+
+}
+
+if(numero2 >
+0) {
+
+console.log("numero2 es positivo");
+
+}
+
+if(numero1 >
+0) {
+
+console.log("numero1 es negativo o distinto de cero");
+
+}
+
+
+
+numero1= 
+numero1 + 1
+
+if(numero1<
+numero2) {
+
+console.log (numero1)
+
+console.log (numero2)
+
+console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+
+}
+
+//Ejercicio 2
+function parImpar(numero) {
+    if (numero%2 == 0){
+       return "Par";
+    }
+   else{
+       return "Impar";
+   }
+}
+console.log(parImpar(2));
+
+//Ejercicio 3 factorial
+function factorial (n) {
+   var total = 1; 
+   for ( var i=1; i<=n; i++) {
+       total = total * i; 
+   }
+   return total; 
+}
+
+console.log(factorial(5));
+
+//Ejercicio 4
+
+
+//Ejercicio 5
